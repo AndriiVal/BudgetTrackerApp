c4a2dd2748d14827776e8d490a40a5eea64b39d9
Andrii Valchuk <andriy.valchuk@gmail.com>
2021-04-15 14:30:07 +0300

    add main files and functions

---
+++ app/static/js/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	$('#datepicker').datepicker({
+	    format: "dd/mm/yyyy",
+	    defaultDate: new Date(),
+	    todayHighlight: true,
+	    todayBtn: "linked",
+	    toggleActive: true
+	});
+	$('#datepicker').datepicker('update', new Date());
+	$('#my_hidden_input').val(
+	        $('#datepicker').datepicker('getFormattedDate')
+    );
+	$('#datepicker').on('changeDate', function() {
+	    $('#my_hidden_input').val(
+	        $('#datepicker').datepicker('getFormattedDate')
+    	);
+    });
+    $('.collapse').removeClass('show');
+})
